b63760b7e820cbea931f416c40fdd647d28f8e3a
ishamjotkaur <ishamjotkaur17@gmail.com>
2025-02-10 18:21:16 -0700

    Set up navigation

--- App.tsx
+++ App.tsx
@@ -5,9 +5,21 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SignIn from './app/SignIn';  
 import Welcome from './app/Welcome';  
+import CityPage from './app/CityPage';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const BottomTabs = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Calgary">
+      {() => <CityPage city="Calgary"  />}
+      </Tab.Screen>
+      <Tab.Screen name="Edmonton">
+      {() => <CityPage city="Edmonton" />}
+      </Tab.Screen>
+  </Tab.Navigator>
+)
 
 export default function App() {
   return (
---
+++ app/CityPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text, Linking, StyleSheet, Image} from "react-native";
+
+const CityPage = ({city}) => {
+ return(
+    <View>
+        <Text>{city} Page</Text>
+    </View>
+ )
+};
+
+export default CityPage;
